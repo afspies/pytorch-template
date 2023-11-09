dab689159b36de4603d784564d2306e11c096352
afspies <alex@afspies.com>
2023-11-09 06:36:14 +0000

    Some fixes

--- scripts/train_network.py
+++ scripts/train_network.py
@@ -1,8 +1,7 @@
 import pytorch-template as project
 from pathlib import Path
-import os.path as op
 
 project.assign_free_gpus(threshold_vram_usage=3500)
 project.set_rng_seeds(seed=47)
 
-data_path = Path(op.join(project.__path__[0], 'data'))
+data_path = Path(project.__path__[0]).parent / "data"
