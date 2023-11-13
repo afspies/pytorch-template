ac81465dd0743f73502fcb2101f44490fe36e35d
afspies <alex@afspies.com>
2023-11-13 07:32:11 +0000

    prevent setup script from modifying git files

--- setup_template.sh
+++ setup_template.sh
@@ -6,6 +6,6 @@ mv pytorch-template/tests/test_pytorch-template.py pytorch-template/tests/test_$
 mv pytorch-template/pytorch-template.py pytorch-template/$PROJECT_NAME.py
 mv pytorch-template $PROJECT_NAME
 
-find ./ -type f -not -path "./setup_template.sh" -exec sed -i -e 's/pytorch-template/'"$PROJECT_NAME"'/g' {} \;
-find ./ -type f -not -path "./setup_template.sh" -exec sed -i -e 's/\[\[AUTHORS\]\]/"'"$AUTHORS"'"/g' {} \;
-find ./ -type f -not -path "./setup_template.sh" -exec sed -i -e 's/\[\[YEAR\]\]/'"$YEAR"'/g' {} \;
+find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" -exec sed -i -e 's/pytorch-template/'"$PROJECT_NAME"'/g' {} \;
+find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" -exec sed -i -e 's/\[\[AUTHORS\]\]/"'"$AUTHORS"'"/g' {} \;
+find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" -exec sed -i -e 's/\[\[YEAR\]\]/'"$YEAR"'/g' {} \;
