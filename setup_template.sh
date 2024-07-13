f52a451bdad4623c8073ba7c8c6ed1d6339269a6
Alex Spies <alex@afspies.com>
2024-07-13 18:03:35 +0900

    Update setup_template.sh

--- setup_template.sh
+++ setup_template.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 AUTHORS="Alex Spies"
 YEAR="2023"
 PROJECT_NAME="pytorch-template"
@@ -6,6 +7,17 @@ mv pytorch-template/tests/test_pytorch-template.py pytorch-template/tests/test_$
 mv pytorch-template/pytorch-template.py pytorch-template/$PROJECT_NAME.py
 mv pytorch-template $PROJECT_NAME
 
-find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" -exec sed -i -e 's/pytorch-template/'"$PROJECT_NAME"'/g' {} \;
-find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" -exec sed -i -e 's/\[\[AUTHORS\]\]/"'"$AUTHORS"'"/g' {} \;
-find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" -exec sed -i -e 's/\[\[YEAR\]\]/'"$YEAR"'/g' {} \;
+# Function to perform safe in-place file editing
+safe_sed() {
+    local pattern="$1"
+    local file="$2"
+    local tmp_file=$(mktemp)
+    sed "$pattern" "$file" > "$tmp_file" && mv "$tmp_file" "$file"
+}
+
+# Process files
+find ./ -type f -not -path "./setup_template.sh" -not -path "*/.git/*" | while read file; do
+    safe_sed "s/pytorch-template/$PROJECT_NAME/g" "$file"
+    safe_sed "s/\[\[AUTHORS\]\]/\"$AUTHORS\"/g" "$file"
+    safe_sed "s/\[\[YEAR\]\]/$YEAR/g" "$file"
+done
